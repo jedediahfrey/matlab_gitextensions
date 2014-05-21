b5ca8422aabc1976e17c80944a25263c9721a838
Jed Frey <github@exstatic.org>
2014-05-20 22:28:07 -0400

    Added BSD 3-clause license.
    Updated ReadMe.

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ matlab_gitextensions
 
 Command wrapper for GitExtensions in Matlab.
 
-%git Control GitExtensions from within MATLAB.
+%GIT - Control GitExtensions from within MATLAB.
 % This is meant to serve as a half way point between the full git bash
 % and TorgoiseGit. It is intended for users that prefer to be able to
 % manipulate git from the MATLAB prompt but also like the added usefullness
@@ -18,5 +18,15 @@ Command wrapper for GitExtensions in Matlab.
 %   git add [GitExtensions Dialog]
 %   git commit [GitExtensions Dialog]
 %
+% Additionally it offers these command aliases:
+%   lasthash      - returns the last commit hash.
+%   currbranch    - returns the current branch
+%   currentbranch - returns the current branch
+%
 % See also: http://code.google.com/p/gitextensions/,
 % http://git-scm.com/documentation
+%
+% GitExtensions is Windows only and by extension so is this script.
+
+% Author:         Jed Frey <github@exstatic.org>
+% Git Repository: https://github.com/jedediahfrey/matlab_gitextensions
--- git.m
+++ git.m
@@ -1,5 +1,5 @@
 function varargout=git(varargin)
-%git Control GitExtensions from within MATLAB.
+%GIT - Control GitExtensions from within MATLAB.
 % This is meant to serve as a half way point between the full git bash
 % and TorgoiseGit. It is intended for users that prefer to be able to
 % manipulate git from the MATLAB prompt but also like the added usefullness
@@ -21,8 +21,25 @@ function varargout=git(varargin)
 %
 % See also: http://code.google.com/p/gitextensions/,
 % http://git-scm.com/documentation
+%
+% GitExtensions is Windows only and by extension so is this script.
 
-% GitExtensions is Windows only.
+% Author:         Jed Frey <github@exstatic.org>
+% Git Repository: https://github.com/jedediahfrey/matlab_gitextensions
+
+% Copyright (c) 2014, Jed Frey
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+% 
+% 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+% 
+% 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+% 
+% 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 if ~ispc
     error('GitExtensions is only available for Windows machines');
 end
